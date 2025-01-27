e1b7924f5b175dee6562059240c17b653e7789ad
Cory McHattie <crmchattie@gmail.com>
2025-01-26 22:42:46 -0500

    Update libraries.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
   rewrites: async () => {
     return [
       {
